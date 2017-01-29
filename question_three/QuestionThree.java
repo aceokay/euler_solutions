1d7ef3af0711905bcc647bf429e53f622c6ab8d2
Austin <aokincaid@gmail.com>
2017-01-28 20:58:24 -0800

    Update Question 3 to a seemingly sound but obviously inefficient solution

--- question_three/QuestionThree.java
+++ question_three/QuestionThree.java
@@ -1,5 +1,3 @@
-import java.util.stream.LongStream;
-
 /**
   * The prime factors of 13195 are 5, 7, 13 and 29.
   *
@@ -7,47 +5,18 @@ import java.util.stream.LongStream;
 */
 public class QuestionThree {
     public static void main(String[] args) {
-        long input = 600851475143l; // Added the 'L' since this was too big
-        // System.out.println(QuestionThree.getPrimesStream(input).max());
-        System.out.println(QuestionThree.getPrimesStream(input));
+        long input = 600851475143l; // Added the 'l' since this was too big for int
+
+        long prime = PrimeStream.getNext();
+        long primeSelection = 0;
+        while (prime < (input / 2)) {
+            if (input % prime == 0) {
+                System.out.println(prime);
+                primeSelection = prime;
+            }
+            prime = PrimeStream.getNext();
+        }
+
+        System.out.println(primeSelection);
     }
-
-    private static long getPrimesStream(long number) {
-        return LongStream.range(2, number + 1)
-                // .filter(x -> x % 2 != 0)
-                .filter(QuestionThree::isPrime)
-                .count();
-                // .filter(x -> QuestionThree.factorOf(x, number));
-    }
-    // private static LongStream getPrimesStream(long number) {
-    //     return LongStream.range(2, number + 1)
-    //             .filter(x -> x % 2 != 0)
-    //             .filter(QuestionThree::isPrime)
-    //             .filter(x -> QuestionThree.factorOf(x, number));
-    // }
-
-    private static boolean isPrime(final long number) {
-      return LongStream.range(2, (long) Math.ceil(Math.sqrt(number + 1)))
-              .noneMatch(divisor -> number % divisor == 0);
-    }
-
-    // We don't care about all primes, so 2 is not required.
-    // private static boolean isPrime(final long number) {
-    //   System.out.println(number);
-    //   return number == 2 || (number % 2 != 0 && LongStream
-    //   .range(2, (long) Math.ceil(Math.sqrt(number + 1)))
-    //   .filter(n -> n % 2 != 0)
-    //   .noneMatch(divisor -> number % divisor == 0)
-    //   );
-    // }
-
-    private static boolean factorOf(long prime, long number) {
-        // System.out.println(number % prime == 0);
-        return number % prime == 0;
-    }
-
-    // @FunctionalInterface
-    // interface FactorInterface<Prime, Number, Return> {
-    //     public Return apply(Prime prime, Number number);
-    // }
 }
